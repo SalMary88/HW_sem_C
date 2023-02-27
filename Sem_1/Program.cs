e765584e85a79b0b4357f34f8e0ba84f33f322ef
SalMary88 <salmariya@bk.ru>
2023-02-27 21:27:01 +0500

    дбавили задачу 2

---
+++ Sem_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b) Console.WriteLine($"max = {a}, min = {b}");
+if (b > a) Console.WriteLine($"max = {b}, min = {a}");
