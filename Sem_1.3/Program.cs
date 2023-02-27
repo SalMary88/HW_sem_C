0d14822d3686602b543e141b81f507a60d1370f7
SalMary88 <salmariya@bk.ru>
2023-02-27 21:37:19 +0500

    дбавили задачу 6

---
+++ Sem_1.3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача  6
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)Console.WriteLine("это четное число");
+else Console.WriteLine("это не четное число");
