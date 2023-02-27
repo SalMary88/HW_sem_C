0c2f789c75cba888e7d0a268e223fc1cef9d4507
SalMary88 <salmariya@bk.ru>
2023-02-27 21:33:30 +0500

    дбавили задачу 4

---
+++ Sem_1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine($"max = {a}");
+if (b > a) Console.WriteLine($"max = {b}");
+if (c > a) Console.WriteLine($"max = {c}");
